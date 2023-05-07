6d59ce5cef0b725a336ad4e84d7559777d660c88
SiapaLupa <muhammadilyasafadhlih@gmail.com>
2023-05-07 20:46:48 +0700

    add functionality

--- script.js
+++ script.js
@@ -1,76 +1,83 @@
 var body = document.body;
 var buttons = document.getElementsByClassName("buttons")[0];
 var textarea = document.getElementsByTagName("textarea")[0];
-
 const OPERATIONS = ["=", "x", "/", "+", "-"];
-const buttonNumber = Array(10).fill().map((_, i) => i).concat(
-  ["C", "Backspace"],
-  OPERATIONS,
-);
-
+const BUTTONS = Array(10)
+    .fill(null)
+    .map((_, i) => String(i))
+    .concat(["C", "Backspace"], OPERATIONS);
 textarea.onkeydown = (e) => {
-  !/\d+|Backspace|Arrow+/.test(e.key) && e.preventDefault()
-}
-
-buttonNumber.forEach((btn) => {
-  let button = document.createElement("button");
-  button.innerText = btn;
-  button.classList.add("button", "button-".concat(!OPERATIONS.find(_ => _ === btn) ? String(btn).toLocaleLowerCase() : validated(btn)))
-  switch (btn) {
-    case "C":
-      button.onclick = () => textarea.value = "";
-      break;
-    case "Backspace":
-      button.onclick = () => textarea.value = textarea.value.slice(0, -1);
-      break;
-    case "x":
-    case "/":
-    case "+":
-    case "-":
-      button.onclick = (e) => {
-        let lastString = textarea.value.slice(-1);
-        !OPERATIONS.find((value) => value === lastString)
-          ? (textarea.value += e.target.innerText)
-          : textarea.value = textarea.value.slice(0, -1) + e.target.innerText;
-      };
-      break;
-    case "=":
-      button.onclick = () => evaluate(textarea.value);
-      break;
-    default:
-      button.onclick = (e) => {
-        textarea.value += e.target.innerText;
-      };
-      break;
-  }
-  // button.addEventListener("click", () => textarea.dispatchEvent(new Event("input")))
-  buttons.appendChild(button);
+    !/\d+|Backspace|Arrow+/.test(e.key) && e.preventDefault();
+};
+BUTTONS.forEach((btn) => {
+    const button = document.createElement("button");
+    const isValidClassName = OPERATIONS.find((_) => _ === btn);
+    button.innerText = btn;
+    button.classList.add("button", "button-".concat(!isValidClassName ? String(btn).toLocaleLowerCase() : validated(btn)));
+    switch (btn) {
+        case "C":
+            button.onclick = () => (textarea.value = "");
+            break;
+        case "Backspace":
+            button.onclick = () => (textarea.value = textarea.value.slice(0, -1));
+            break;
+        case "x":
+        case "/":
+        case "+":
+        case "-":
+            button.onclick = (evt) => {
+                let lastString = textarea.value.slice(-1);
+                let target = evt.target;
+                !OPERATIONS.find((value) => value === lastString)
+                    ? (textarea.value += target.innerText)
+                    : (textarea.value = textarea.value.slice(0, -1) + target.innerText);
+            };
+            break;
+        case "=":
+            button.onclick = () => textarea.value = String(evaluate(textarea.value));
+            break;
+        default:
+            button.onclick = (evt) => {
+                let target = evt.target;
+                textarea.value += target.innerText;
+            };
+            break;
+    }
+    buttons.appendChild(button);
 });
-
 function validated(string) {
-  let toText = ""
-  switch (string) {
-    case "x": toText = "multiply"
-      break;
-    case "/": toText = "divide"
-      break;
-    case "+": toText = "plus"
-      break;
-    case "-": toText = "minus"
-      break;
-    case "=": toText = "equals"
-      break;
-    default: toText = "cannot change to text"
-  }
-  return toText
+    const OPERATIONS_TEXT = {
+        "=": "equals",
+        "x": "multiply",
+        "/": "divide",
+        "+": "plus",
+        "-": "minus",
+    };
+    return OPERATIONS_TEXT[string];
 }
-
 function evaluate(rawValue) {
-  const arrValue = rawValue.match(/\d+|\+|\-|\x|\//g)
-  const number = arrValue.filter(_ => /\d+/.test(_))
-  const operation = arrValue.filter(_ => /\+|\-|\x|\//g.test(_))
-  console.log(number);
-  console.log(operation);
-  a = operation.findIndex(_=>/\x|\//g.test(_)) 
+    const arrValue = rawValue.match(/\d+|\+|\-|\x|\//g);
+    const operand = arrValue.filter((_) => /\d+/.test(_)).map((_) => Number(_));
+    const operation = arrValue.filter((_) => /\+|\-|\x|\//g.test(_));
+    let index = 0;
+    while (index < operation.length) {
+        if (["x", "/"].find((_) => _ === operation[index])) {
+            operand.splice(index, 2, operate(operand[index], operation[index], operand[index + 1]));
+            operation.splice(index, 1);
+        }
+        else
+            index++;
+    }
+    index = 0;
+    let result = operand.reduce((prev, curr, index) => operate(prev, operation[index - 1], curr));
+    return result;
 }
-
+function operate(firstNumber, operator, secondNumber) {
+    const result = {
+        "x": firstNumber * secondNumber,
+        "/": firstNumber / secondNumber,
+        "+": firstNumber + secondNumber,
+        "-": firstNumber - secondNumber,
+    };
+    return result[operator];
+}
---
+++ script.ts
@@ -0,0 +1,111 @@
+var body = document.body as HTMLBodyElement;
+var buttons = document.getElementsByClassName(
+  "buttons",
+)[0] as HTMLButtonElement;
+var textarea = document.getElementsByTagName(
+  "textarea",
+)[0] as HTMLTextAreaElement;
+
+const OPERATIONS: Array<string> = ["=", "x", "/", "+", "-"];
+
+const BUTTONS: Array<string> = Array(10)
+  .fill(null)
+  .map((_, i) => String(i))
+  .concat(["C", "Backspace"], OPERATIONS);
+
+textarea.onkeydown = (e: KeyboardEvent) => {
+  !/\d+|Backspace|Arrow+/.test(e.key) && e.preventDefault();
+};
+BUTTONS.forEach((btn: string) => {
+  const button: HTMLButtonElement = document.createElement("button");
+  const isValidClassName: string | undefined = OPERATIONS.find((_) =>
+    _ === btn
+  );
+
+  button.innerText = btn;
+  button.classList.add(
+    "button",
+    "button-".concat(
+      !isValidClassName ? String(btn).toLocaleLowerCase() : validated(btn),
+    ),
+  );
+  switch (btn) {
+    case "C":
+      button.onclick = () => (textarea.value = "");
+      break;
+    case "Backspace":
+      button.onclick = () => (textarea.value = textarea.value.slice(0, -1));
+      break;
+    case "x":
+    case "/":
+    case "+":
+    case "-":
+      button.onclick = (evt: MouseEvent) => {
+        let lastString: string = textarea.value.slice(-1);
+        let target = evt.target as HTMLElement;
+        !OPERATIONS.find((value) => value === lastString)
+          ? (textarea.value += target.innerText)
+          : (textarea.value = textarea.value.slice(0, -1) + target.innerText);
+      };
+      break;
+    case "=":
+      button.onclick = () => textarea.value = String(evaluate(textarea.value));
+      break;
+    default:
+      button.onclick = (evt: MouseEvent) => {
+        let target = evt.target as HTMLElement;
+        textarea.value += target.innerText;
+      };
+      break;
+  }
+  buttons.appendChild(button);
+});
+
+function validated(string: string): string {
+  const OPERATIONS_TEXT: Record<string, string> = {
+    "=": "equals",
+    "x": "multiply",
+    "/": "divide",
+    "+": "plus",
+    "-": "minus",
+  };
+  return OPERATIONS_TEXT[string];
+}
+
+function evaluate(rawValue: string): number {
+  const arrValue: RegExpMatchArray = rawValue.match(/\d+|\+|\-|\x|\//g)!;
+  const operand: Array<number> = arrValue.filter((_) => /\d+/.test(_)).map(
+    (_) => Number(_),
+  );
+  const operation: Array<string> = arrValue.filter((_) => /\+|\-|\x|\//g.test(_));
+  let index = 0;
+  while (index < operation.length) {
+    if (["x", "/"].find((_) => _ === operation[index])) {
+      operand.splice(
+        index,
+        2,
+        operate(operand[index], operation[index], operand[index + 1]),
+      );
+      operation.splice(index, 1);
+    } else index++;
+  }
+  index = 0;
+  let result = operand.reduce((prev: number, curr: number, index: number) =>
+    operate(prev, operation[index - 1], curr)
+  );
+  return result;
+}
+
+function operate(
+  firstNumber: number,
+  operator: string,
+  secondNumber: number,
+): number {
+  const result: Record<string, number> = {
+    "x": firstNumber * secondNumber,
+    "/": firstNumber / secondNumber,
+    "+": firstNumber + secondNumber,
+    "-": firstNumber - secondNumber,
+  };
+  return result[operator];
+}
